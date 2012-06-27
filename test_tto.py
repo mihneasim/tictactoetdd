2473bd4ab8d4e90a37ca802dbd1eb3b3ffda1afe
mihneasim <contact@mesimian.com>
2012-06-27 20:10:41 +0300

    test consistent board in RED

--- test_tto.py
+++ test_tto.py
@@ -10,6 +10,12 @@ class TickTackToeTestSuite(unittest.TestCase):
                  [' ', ' ', ' '],
                  [' ', ' ', ' '],]
 
+    def read_user_input(self, user_input):
+        return map(int, user_input.split(","))
+
+    def move(self, board, x, y, who):
+        board[x][y] =  who
+
     def test_first_cell_empty(self):
         """ Test first cell is empty """
         board = self.init_board()
@@ -26,8 +32,8 @@ class TickTackToeTestSuite(unittest.TestCase):
         """ Test that after first read, one cell is filled """
         board = self.init_board()
         user_input = '0,1'
-        x, y = map(int, user_input.split(","))
-        board[x][y] = 'x'
+        x, y = self.read_user_input(user_input)
+        self.move(board, x, y, 'x')
         counter = 0
         for row in board:
             for cell in row:
@@ -38,7 +44,26 @@ class TickTackToeTestSuite(unittest.TestCase):
     def test_requested_cell_is_occupied(self):
         """ Testing a cell requested by user to move is already taken """
         board = self.init_board()
-        board[1][2] = 'x'
         user_input = '1,2'
-        x, y = map(int, user_input.split(","))
+        x, y = self.read_user_input(user_input)
+        self.move(board, x, y, 'x')
         self.assertTrue(board[x][y] != ' ')
+
+    def test_board_consistent_state(self):
+        """ Tests board is in consistent state """
+        board = self.init_board()
+
+        self.move(board, 1, 2, 'x')
+        self.move(board, 1, 0, 'x')
+        self.move(board, 1, 1, 'x')
+        self.move(board, 2, 2, '0')
+
+        cnt_x = cnt_o = 0
+        for row in board:
+            for cell in row:
+                if cell == 'x':
+                    cnt_x += 1
+                if cell == 'o':
+                    cnt_o += 1
+
+        self.assertTrue((cnt_x - cnt_o) in (0, 1))
