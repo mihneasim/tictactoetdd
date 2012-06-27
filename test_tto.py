c65b7cf3f5b6ce46bc8247cf5e776891a70543de
mihneasim <contact@mesimian.com>
2012-06-27 19:39:54 +0300

    random cell test passes

--- test_tto.py
+++ test_tto.py
@@ -1,11 +1,20 @@
 import unittest
-
+import random
 
 
 class TickTackToeTestSuite(unittest.TestCase):
 
 
-    def test_get_cell(self):
+    def test_first_cell_empty(self):
         """ Test first cell is empty """
         board = [[' ']]
         self.assertEqual(board[0][0], ' ')
+
+    def test_random_cell_is_empty(self):
+        """ test random cell is empty """
+        board = [[' ', ' ', ' '],
+                 [' ', ' ', ' '],
+                 [' ', ' ', ' '],]
+        rand_row = random.randint(0, 2)
+        rand_col = random.randint(0, 2)
+        self.assertEqual(board[rand_row][rand_col], ' ')
